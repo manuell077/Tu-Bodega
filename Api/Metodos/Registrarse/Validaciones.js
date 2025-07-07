3a904c6125d016f265a3ed02ce96229f8a32584a
Manuel Ardila <ardilasanchezmanuelsteven@gmail.com>
2025-07-07 11:56:28 -0500

    Logre hacer validaciones de campos vacios

--- Api/Metodos/Registrarse/Validaciones.js
+++ Api/Metodos/Registrarse/Validaciones.js
@@ -1,19 +1,24 @@
-export function ValidarNombre(e,input,div) {
+export function ValidarVacios(e) {
         
-      if(!input.value){ //Si no tiene ningun valor se crea un mensaje de error 
+      if(!e.target.value.trim()){ //Si no tiene ningun valor se crea un mensaje de error 
         
+        if(e.target.nextSibling){
+           
+          e.target.nextSibling.remove()
+
+        }
+         console.log("LLeno")
         let error = document.createElement("span") 
         error.classList.add("mensajeError")
-        error.textContent = "El campo nombre debe  completarse" 
-        div.insertAdjacentElement("afterend",error)  //Se inserta debajo del input
-        div.classList.add("componente--sinMargin")
-
-        
-        
-        
+        error.textContent = `El campo ${e.target.name} debe  completarse` 
+        e.target.insertAdjacentElement("afterend",error)  //Se inserta debajo del input 
+      }else{
+        console.log("Valor ")
+          e.target.nextSibling.remove()
       }
-      
-
-
-
 }
+
+
+
+
+
--- Api/Registrarme.js
+++ Api/Registrarme.js
@@ -1,14 +1,24 @@
-import {post,ValidarNombre} from "./Metodos/Registrarse/index.js" //Se importa el metodo post del archivo barril
+import {post,ValidarVacios} from "./Metodos/Registrarse/index.js" //Se importa el metodo post del archivo barril
 
 const formu = document.querySelector(".registrarme") //Se selecciona el formulario 
 const nombre = document.querySelector("#nombre") //Se selecciona el input con el id nombre 
-const componente = document.querySelector(".componente") //Se selecciona el div que tiene los labels y inputs
-
+const password = document.querySelector("#contrasena") //Se selecciona el input con el id de contraseña
+const Repeatpassword = document.querySelector("#repetirContrasena") //Se selecciona el input con el id de Repetir contraseña
+const correo =  document.querySelector("#correoElectronico") //Se selecciona el input con el id de correo
+const telefono = document.querySelector("#telefono") //Se selecciona el input con el id del telefono
+const cedula = document.querySelector("#cedula") //Se selecciona el input con el id de la cedula
+const direccion = document.querySelector("#direccion") //Se selecciona el input con el id de la cedula
 
 formu.addEventListener("submit", (e) =>   post(e,formu)) //Se le añade el evento al formulario que cuando se envie se ejecute la funcion 
 
 
-//Se realiza las validaciones con el evento keypress que es un evento que funciona cuando se presiona una tecla que produce un caracter imprimible 
-formu.addEventListener("keypress",(e) => ValidarNombre(e,nombre,componente)) 
+//Se realiza las validaciones con el evento keyup que es un evento que se activa cuando el usuario suelta una tecla 
 
+//Validaciones para saber si los campos estan vacios 
+nombre.addEventListener("keyup",ValidarVacios) 
+
+
+
+//Evento de desenfoque que es cuando se quite el foco de este campo entonces borre el mensaje de error
+// nombre.addEventListener("blur",EliminarErrores)
 
