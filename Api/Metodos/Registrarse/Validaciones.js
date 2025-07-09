7874437229f055bbe0c054b440f561ea24373af2
Manuel Ardila <ardilasanchezmanuelsteven@gmail.com>
2025-07-09 06:58:14 -0500

    Cambios desde el sena

--- Api/Metodos/Registrarse/Validaciones.js
+++ Api/Metodos/Registrarse/Validaciones.js
@@ -2,19 +2,16 @@ export function ValidarVacios(e) {
         
       if(!e.target.value.trim()){ //Si no tiene ningun valor se crea un mensaje de error 
         
-        if(e.target.nextSibling){
-           
-          e.target.nextSibling.remove()
-
-        }
-         console.log("LLeno")
+        
+        
         let error = document.createElement("span") 
         error.classList.add("mensajeError")
         error.textContent = `El campo ${e.target.name} debe  completarse` 
         e.target.insertAdjacentElement("afterend",error)  //Se inserta debajo del input 
       }else{
-        console.log("Valor ")
+        if(e.target.nextSibling){
           e.target.nextSibling.remove()
+        }
       }
 }
 
